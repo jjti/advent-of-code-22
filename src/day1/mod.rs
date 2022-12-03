976a77fcba28d787726ca8136d23dfd983c0131f
josh <joshua.timmons1@gmail.com>
2022-12-02 21:27:46 -0500

    day 1/2

---
+++ src/day1/mod.rs
@@ -0,0 +1,24 @@
+use std::fs::read_to_string;
+
+pub fn solve() -> String {
+    let file = read_to_string("src/day1/input.txt").unwrap();
+
+    let mut vals: Vec<i32> = Vec::new();
+    let mut cur = 0;
+    for line in file.split('\n') {
+        if line.is_empty() {
+            vals.push(cur);
+            cur = 0;
+        } else {
+            cur += line.parse::<i32>().unwrap();
+        }
+    }
+    vals.sort();
+    vals.reverse();
+
+    format!(
+        "{} {}",
+        vals.first().unwrap(),
+        vals.iter().take(3).sum::<i32>()
+    )
+}
---
+++ src/day2/mod.rs
@@ -0,0 +1,91 @@
+use std::fs::read_to_string;
+
+pub fn part_1() -> String {
+    let input = read_to_string("src/day2/input.txt").unwrap();
+
+    let mut sum = 0;
+    for line in input.split('\n') {
+        let mut cols = line.split_ascii_whitespace();
+        let first = cols.next().unwrap();
+        let last = cols.next().unwrap();
+
+        match last {
+            "X" => {
+                sum += 1;
+
+                match first {
+                    "A" => sum += 3,
+                    "B" => {}
+                    "C" => sum += 6,
+                    _ => panic!(),
+                }
+            }
+            "Y" => {
+                sum += 2;
+
+                match first {
+                    "A" => sum += 6,
+                    "B" => sum += 3,
+                    "C" => {}
+                    _ => panic!(),
+                }
+            }
+            "Z" => {
+                sum += 3;
+
+                match first {
+                    "A" => {}
+                    "B" => sum += 6,
+                    "C" => sum += 3,
+                    _ => panic!(),
+                }
+            }
+            _ => panic!(),
+        }
+    }
+
+    format!("{}", sum)
+}
+
+pub fn part_2() -> String {
+    let input = read_to_string("src/day2/input.txt").unwrap();
+
+    let mut sum = 0;
+    for line in input.split('\n') {
+        let mut cols = line.split_ascii_whitespace();
+        let first = cols.next().unwrap();
+        let last = cols.next().unwrap();
+
+        match last {
+            "X" => match first {
+                "A" => sum += 3,
+                "B" => sum += 1,
+                "C" => sum += 2,
+                _ => panic!(),
+            },
+            "Y" => {
+                sum += 3;
+
+                match first {
+                    "A" => sum += 1,
+                    "B" => sum += 2,
+                    "C" => sum += 3,
+                    _ => panic!(),
+                }
+            }
+            "Z" => {
+                sum += 6;
+
+                match first {
+                    "A" => sum += 2,
+                    "B" => sum += 3,
+                    "C" => sum += 1,
+                    _ => panic!(),
+                }
+            }
+            _ => panic!(),
+        }
+    }
+
+    format!("{}", sum)
+}
